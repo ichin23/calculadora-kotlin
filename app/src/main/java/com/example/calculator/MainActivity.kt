2ae0dd36c4580cbe107fecc043ea9eaa61f2b2da
ichin23 <pedroferreiramanoel@gmail.com>
2024-10-07 21:25:50 -0300

    adicionando stilo

--- app/src/main/java/com/example/calculator/MainActivity.kt
+++ app/src/main/java/com/example/calculator/MainActivity.kt
@@ -12,7 +12,8 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
@@ -23,8 +24,8 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.calculator.ui.theme.CalculatorTheme
@@ -49,16 +50,33 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Calculator(modifier : Modifier){
+    var resultado by remember { mutableStateOf(false) }
+
     var exp by remember { mutableStateOf("") }
 
+    fun add(str:String){
+        println(resultado)
+        if (resultado) {
+            exp=str
+            resultado=false
+        }else{
+            exp+=str
+        }
+
+    }
+
     Column(modifier = modifier.padding(16.dp)) {
-        Box(Modifier.height(250.dp).fillMaxWidth()){Text(
-            text = exp,fontSize = 40.sp,
+        Box(
+            Modifier
+                .height(250.dp)
+                .padding(vertical = 8.dp)
+                .fillMaxWidth()){
+            Text(
+            text = exp, fontSize = 55.sp, fontWeight = FontWeight.W600,
             modifier = Modifier.align(Alignment.BottomEnd),
-maxLines = 1,
-
-
+            maxLines = 1,
         )}
+
         Column(
             modifier = Modifier.fillMaxSize(),
             verticalArrangement = Arrangement.SpaceBetween,
@@ -68,47 +86,52 @@ maxLines = 1,
                 modifier = Modifier.fillMaxWidth(),
                 horizontalArrangement = Arrangement.SpaceBetween
             ) {
-                Botao(value = "C", onClick = { exp=""})
-                Botao(value = "(", onClick = { exp+="("})
-                Botao(value = ")", onClick = { exp+=")"})
-                Botao(value = "+", onClick = { exp+="+"})
+                Botao(value = "C", color = Color(0xfff77e7e), onClick = { add("")})
+                Botao(value = "(", onClick = {
+
+                    add("(")
+                })
+                Botao(value = ")", onClick = { add(")")})
+                Botao(value = "+",color=Color(0xff84acf0), onClick = { exp+="+"})
             }
             Row(
                 modifier = Modifier.fillMaxWidth(),
                 horizontalArrangement = Arrangement.SpaceBetween
             ) {
-                Botao(value = 7, onClick = { exp+=7})
-                Botao(value = 8, onClick = { exp+=8})
-                Botao(value = 9, onClick = { exp+=9})
-                Botao(value = "-", onClick = { exp+="-"})
+                Botao(value = 7, onClick = { add("7")})
+                Botao(value = 8, onClick = { add("8")})
+                Botao(value = 9, onClick = { add("9")})
+                Botao(value = "-",color=Color(0xff84acf0), onClick = { exp+="-"})
             }
             Row(modifier = Modifier.fillMaxWidth(),
                 horizontalArrangement = Arrangement.SpaceBetween
             ) {
-                Botao(value = 4, onClick = { exp+=4})
-                Botao(value = 5, onClick = { exp+=5})
-                Botao(value = 6, onClick = { exp+=6})
-                Botao(value = "/", onClick = { exp+="/"})
+                Botao(value = 4, onClick = { add("4")})
+                Botao(value = 5, onClick = { add("5")})
+                Botao(value = 6, onClick = { add("6")})
+                Botao(value = "/",color=Color(0xff84acf0), onClick = { exp+="/"})
             }
             Row(modifier = Modifier.fillMaxWidth(),
                 horizontalArrangement = Arrangement.SpaceBetween
             ) {
-                Botao(value = 1, onClick = { exp+=1})
-                Botao(value = 2, onClick = { exp+=2})
-                Botao(value = 3, onClick = { exp+=3})
-                Botao(value = "*", onClick = { exp+="*"})
+                Botao(value = 1, onClick = { add("1")})
+                Botao(value = 2, onClick = { add("2")})
+                Botao(value = 3, onClick = { add("3")})
+                Botao(value = "*", color=Color(0xff84acf0), onClick = { exp+="*"})
             }
             Row(modifier = Modifier.fillMaxWidth(),
                 horizontalArrangement = Arrangement.SpaceBetween
             ) {
-                Botao(value = ".", onClick = { exp+="."})
-                Botao(value = 0, onClick = { exp+=0})
-                Botao(value = "", onClick = { })
-                Botao(value = "=", onClick = {
+                Botao(value = ".", onClick = { add(".")})
+                Botao(value = 0, onClick = { add("0")})
+                Botao(value = "<-",color=Color(0xff84acf0), onClick = {exp=exp.dropLast(1) })
+                Botao(value = "=", color=Color(0xff366ecf), onClick = {
+                    resultado=true
                     exp = Expression(exp).calculate().toString()
                     if (exp.split(".")[1] =="0"){
                         exp = exp.split(".")[0]
                     }
+
                 })
             }
         }
@@ -116,24 +139,12 @@ maxLines = 1,
 }
 
 @Composable
-fun Botao(value: Any, onClick:()->Unit){
-    TextButton(onClick = onClick) {
-        Text(text = "$value", fontSize = 30.sp)
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-            text = "Hello $name!",
-            modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    CalculatorTheme {
-        Greeting("Android")
+fun Botao(value: Any, onClick:()->Unit, color: Color=Color(0xffcad7ed)){
+    TextButton(onClick = onClick,
+        colors= ButtonDefaults.textButtonColors(
+            containerColor = color
+        ),
+        shape = RoundedCornerShape(50)) {
+        Text(text = "$value", fontSize = 30.sp, color = Color.Black)
     }
 }
